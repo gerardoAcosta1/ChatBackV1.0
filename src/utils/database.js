d28fd90b56b03424075327b706d060f45aa5351f
GerardoAcosta1 <gerardonuevo1234321@gmail.com>
2023-10-19 13:18:11 -0600

    se mi

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,14 +1,16 @@
 import { Sequelize } from "sequelize"
 import "dotenv/config.js";
-const db = new Sequelize( 
-  { host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    dialect: 'postgres',
-  });
-  
+
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 export default db
 
 /*
@@ -23,5 +25,16 @@ const db = new Sequelize(process.env.DATABASE_URL, {
     },
   });
 
+export default db
+
+const db = new Sequelize( 
+  { host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: 'postgres',
+  });
+  
 export default db
 */
