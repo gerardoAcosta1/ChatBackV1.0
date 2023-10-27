38bfcf7479713f12f2e422a42e0f7b899725e766
GerardoAcosta1 <gerardonuevo1234321@gmail.com>
2023-10-26 19:01:33 -0600

    servidor funcionando sender

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,14 +1,15 @@
 import { Sequelize } from "sequelize"
 import "dotenv/config.js";
 
-const db = new Sequelize( 
-  { host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    dialect: 'postgres',
-  });
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 async function initializeDatabase() {
   try {
